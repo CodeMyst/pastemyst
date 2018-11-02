f876164a6cbbbe8465ae3ece97546f95668f136f
CodeMyst <codemyst@outlook.com>
2018-11-02 20:51:31 +0100

    Added an API, code encoded into base64

--- source/app.d
+++ source/app.d
@@ -1,3 +1,5 @@
+module pastemyst;
+
 import ddbc;
 import std.stdio;
 import vibe.vibe;
@@ -10,6 +12,7 @@ import std.conv;
 import etc.c.sqlite3;
 import ddbc.drivers.sqliteddbc;
 import std.uri;
+import std.base64;
 
 const string connectionString = "sqlite:pastemysts.sqlite";
 SQLITEConnection connection;
@@ -19,7 +22,32 @@ void showError (HTTPServerRequest req, HTTPServerResponse res, HTTPServerErrorIn
 	res.render!("error.dt", req, error);
 }
 
-final class PasteMyst
+interface IRest
+{
+	@bodyParam ("code", "code")
+	@method (HTTPMethod.POST)
+	@path ("/api/paste")
+	Json postPaste (string code) @safe;
+
+	@queryParam ("id", "id")
+	Json getPaste (long id) @safe;
+}
+
+@rootPathFromName
+class Api : IRest
+{
+	Json postPaste (string code) @trusted
+	{
+		return createPaste (code).serializeToJson;
+	}
+
+	Json getPaste (long id) @trusted
+	{
+		return pastemyst.getPaste (id).serializeToJson;
+	}
+}
+
+class PasteMyst
 {
 	// GET /
 	void get ()
@@ -30,46 +58,69 @@ final class PasteMyst
 	// POST /paste
 	void postPaste (string code)
 	{
-		auto stmt = connection.createStatement();
-		scope(exit) stmt.close();
+		PasteMystInfo info = createPaste (code);
 
-		immutable long createdAt = Clock.currTime.toUnixTime;
-
-		stmt.executeUpdate("INSERT INTO PasteMysts (createdAt, code) VALUES
-							(" ~ to!string (createdAt) ~ ", \"" ~ code ~ "\")");
-
-		sqlite3_int64 id = sqlite3_last_insert_rowid (connection.getConnection ());
-
-		redirect ("paste?id=" ~ to!string (id));
+		redirect ("paste?id=" ~ to!string (info.id));
 	}
 
 	// GET /paste
 	void getPaste (long id)
 	{
-		auto stmt = connection.createStatement ();
-		scope(exit) stmt.close ();
+		PasteMystInfo info = pastemyst.getPaste (id);
 
-		auto rs = stmt.executeQuery("SELECT id, createdAt, code FROM PasteMysts WHERE id='" ~ to!string (id) ~ "'");
-
-		long unixTime;
-		string code;
-
-		while (rs.next)
-		{
-			unixTime = to!long (rs.getLong (2));
-			code = decode (to!string (rs.getString (3)));
-		}
-
-		immutable string createdAt = SysTime.fromUnixTime (unixTime, UTC ()).toUTC.toString [0..$-1];
+		immutable string createdAt = SysTime.fromUnixTime (info.createdAt, UTC ()).toUTC.toString [0..$-1];
+		immutable string code = cast (string) Base64.decode (info.code);
 
 		render!("paste.dt", id, createdAt, code);
 	}
+}
+
+PasteMystInfo createPaste (string code)
+{
+	auto stmt = connection.createStatement ();
+	scope (exit) stmt.close ();
+
+	immutable long createdAt = Clock.currTime.toUnixTime;
+
+	stmt.executeUpdate ("INSERT INTO PasteMysts (createdAt, code) VALUES
+						(" ~ to!string (createdAt) ~ ", \"" ~ code ~ "\")");
+
+	sqlite3_int64 id = sqlite3_last_insert_rowid (connection.getConnection ());
+
+	return PasteMystInfo (id, createdAt, code);
+}
+
+PasteMystInfo getPaste (long id)
+{
+	auto stmt = connection.createStatement ();
+	scope(exit) stmt.close ();
+
+	auto rs = stmt.executeQuery("SELECT id, createdAt, code FROM PasteMysts WHERE id='" ~ to!string (id) ~ "'");
+
+	PasteMystInfo info;
+	info.id = id;
+
+	while (rs.next)
+	{
+		info.createdAt = to!long (rs.getLong (2));
+		info.code = to!string (rs.getString (3));
+	}
+
+	return info;
+}
+
+struct PasteMystInfo
+{
+	long id;
+	long createdAt;
+	string code;
 }
 
 void main ()
 {
 	auto router = new URLRouter;
 	router.registerWebInterface (new PasteMyst);
+	router.registerRestInterface (new Api);
 	router.get ("*", serveStaticFiles ("public"));
 
 	auto settings = new HTTPServerSettings;

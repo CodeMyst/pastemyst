3c5edcf213964967a39eec93f27744792206c516
CodeMyst <codemyst@outlook.com>
2019-01-08 12:54:39 +0100

    Able to get paste without /paste?id= url

--- source/app.d
+++ source/app.d
@@ -19,9 +19,9 @@ void main ()
 	import pastemyst : WebInterface, RestApiInterface, initialize, deleteExpiredPasteMysts;
 
 	auto router = new URLRouter;
+	router.get ("*", serveStaticFiles ("public"));
 	router.registerWebInterface (new WebInterface);
 	router.registerRestInterface (new RestApiInterface);
-	router.get ("*", serveStaticFiles ("public"));
 
 	auto settings = new HTTPServerSettings;
 	settings.bindAddresses = ["127.0.0.1"];

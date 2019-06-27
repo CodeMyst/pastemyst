84f217b9d006f93f6da8321fb8ce66940fbe8b38
CodeMyst <codemyst@outlook.com>
2019-06-27 21:18:32 +0200

    Displaying a login modal about features

--- frontend/src/scripts/router/viewComponent.ts
+++ frontend/src/scripts/router/viewComponent.ts
@@ -1,4 +1,8 @@
+import Page from "page";
+
 export default interface IViewComponent
 {
+    parent: Page;
+
     run (): Promise<void>;
 }
--- frontend/src/scripts/views/components/navigation.ts
+++ frontend/src/scripts/views/components/navigation.ts
@@ -1,18 +1,37 @@
 import IViewComponent from "router/viewComponent";
 import { isLoggedIn } from "api/auth";
+import Page from "router/page";
+import Modal from "modal";
 
 export default class Navigation implements IViewComponent
 {
+    public parent: Page;
+
     public async run (): Promise<void>
     {
         const loggedIn: boolean = await isLoggedIn ();
+        
+        const loginElement: HTMLElement = document.getElementById ("login");
 
         if (loggedIn)
         {
-            const loginElement: Element = document.getElementById ("login");
-            loginElement.removeAttribute ("href");
             loginElement.setAttribute ("route", "/profile");
             loginElement.textContent = "profile";
+        }
+        else
+        {
+            this.parent.viewComponents.forEach ((component: IViewComponent) =>
+            {
+                const loginModal: Modal = component as Modal;
+
+                if (loginModal !== null && loginModal.name === "login-modal")
+                {
+                    loginElement.addEventListener ("click", () =>
+                    {
+                        loginModal.open ();
+                    });
+                }
+            });
         }
     }
 }

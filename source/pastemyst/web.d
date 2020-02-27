82488a7ade32e9e0cce2da4fc3e90aee94db0363
Daniel Munch <daniellmunch@gmail.com>
2020-02-27 15:50:24 +0100

    Added viewing of raw pasties

--- source/pastemyst/web.d
+++ source/pastemyst/web.d
@@ -20,4 +20,25 @@ public class WebInterface
 
 		render!("home.dt", expires, languages);
 	}
+
+	@path("/raw/:id/:index")
+	public void getRawPasty(string _id, int _index)
+	{
+		import pastemyst.db : findOneById;
+		import pastemyst.data : Paste;
+		
+		const int index = _index - 1;
+
+		const auto paste = findOneById!Paste(_id);
+		enforceHTTP(!paste.isNull, HTTPStatus.notFound, "invalid paste id.");
+		enforceHTTP(!(index + 1 > paste.get().pasties.length || index < 0), HTTPStatus.notFound, "invalid pasty index.");
+
+		const auto pasty = paste.get().pasties[index];
+		const string pasteTitle = paste.get().title == "" ? "untitled" : paste.get().title;
+		const string pastyTitle = pasty.title == "" ? "untitled" : pasty.title;
+		const string title = pasteTitle ~ " - " ~ pastyTitle;
+		const string rawCode = pasty.code;
+
+		render!("raw.dt", title, rawCode);
+	}
 }

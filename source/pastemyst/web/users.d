70c0d4cdcd4b11e66401bf1d00e19b8a559ebf5e
CodeMyst <codemyst@outlook.com>
2020-04-26 22:44:15 +0200

    added public profile page

---
+++ source/pastemyst/web/users.d
@@ -0,0 +1,42 @@
+module pastemyst.web.users;
+
+import vibe.d;
+import pastemyst.data;
+
+/++
+ + web interface for the /users endpoint
+ +/
+@path("/users")
+public class UsersWeb
+{
+    @path("/:username")
+    public void getUser(HTTPServerRequest req, string _username)
+    {
+        import pastemyst.db : findOne;
+
+        auto userRes = findOne!User(["username": _username]);
+
+        if (userRes.isNull())
+        {
+            return;
+        }
+
+        const user = userRes.get();
+
+        if (!user.publicProfile)
+        {
+            return;
+        }
+
+        UserSession session = UserSession.init;
+
+        if (req.session && req.session.isKeySet("user"))
+        {
+            session = req.session.get!UserSession("user");    
+        }
+
+        string title = user.username ~ " - public profile";
+
+        render!("publicProfile.dt", session, title, user);
+    }
+}

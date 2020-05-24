bb45fdcad54c71e33f430de0c5e60f785bacebf1
CodeMyst <codemyst@outlook.com>
2020-05-24 13:28:23 +0200

    added fullwidth page toggle

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,38 @@
 import "./components/navigation.js";
+
+window.addEventListener("load", () =>
+{
+    document.querySelector("nav .fullwidth").addEventListener("click", toggleFullwidth);
+
+    setFullwidthClasses();
+});
+
+function toggleFullwidth()
+{
+    let fullwidth = localStorage.getItem("fullwidth") === "true";
+
+    localStorage.setItem("fullwidth", !fullwidth);
+
+    setFullwidthClasses();
+}
+
+function setFullwidthClasses()
+{
+    let fullwidth = localStorage.getItem("fullwidth") === "true";
+    let container = document.getElementById("container");
+
+    if (fullwidth)
+    {
+        if (!container.classList.contains("fullwidth"))
+        {
+            container.classList.add("fullwidth");
+        }
+    }
+    else
+    {
+        if (container.classList.contains("fullwidth"))
+        {
+            container.classList.remove("fullwidth");
+        }
+    }
+}

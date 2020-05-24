bb051bb63d48172ac6a1e0db38041b27fbfbc3b1
CodeMyst <codemyst@outlook.com>
2020-05-24 14:35:53 +0200

    added word wrap toggle

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,19 +1,36 @@
 import "./components/navigation.js";
+import { getWordwrap, getFullwidth } from "./helpers/options.js";
 
 window.addEventListener("load", () =>
 {
     document.querySelector("nav .fullwidth").addEventListener("click", toggleFullwidth);
+    document.querySelector("nav .wordwrap").addEventListener("click", toggleWordwrap);
 
     setFullwidthClasses();
 });
 
 function toggleFullwidth()
 {
-    let fullwidth = localStorage.getItem("fullwidth") === "true";
+    let fullwidth = getFullwidth();
 
     localStorage.setItem("fullwidth", !fullwidth);
 
     setFullwidthClasses();
+}
+
+function toggleWordwrap()
+{
+    let wordwrap = getWordwrap();
+
+    localStorage.setItem("wordwrap", !wordwrap);
+
+    let editorElements = document.querySelectorAll(".CodeMirror");
+
+    for (let i = 0; i < editorElements.length; i++)
+    {
+        let editor = editorElements[i].CodeMirror;
+        editor.setOption("lineWrapping", !wordwrap);
+    }
 }
 
 function setFullwidthClasses()

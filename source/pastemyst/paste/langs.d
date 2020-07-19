be4802522f6fb9ba8fa423463e92b333780dd11b
CodeMyst <codemyst@outlook.com>
2020-07-19 19:27:21 +0200

    added a language bar to the profile and paste page

---
+++ source/pastemyst/paste/langs.d
@@ -0,0 +1,51 @@
+module pastemyst.paste.langs;
+
+import pastemyst.data;
+
+struct LangStat
+{
+    string lang;
+    float perc;
+}
+
+/++
+ + calculates the language stats of a paste
+ + returns an associative array with the language name as the key and the percentage as the value
+ +/
+public LangStat[] pasteLangStats(const Paste paste) @safe
+{
+    import std.algorithm : sort;
+    import std.array : array;
+
+    LangStat[] res;
+
+    ulong[string] chars;
+    ulong total;
+
+    foreach (pasty; paste.pasties)
+    {
+        if (!(pasty.language in chars))
+        {
+            chars[pasty.language] = 0;
+        }
+
+        chars[pasty.language] += pasty.code.length;
+        total += pasty.code.length;
+    }
+
+    if (total == 0)
+    {
+        return res;
+    }
+
+    foreach (lang, c; chars)
+    {
+        float perc = (c / cast(float) total) * 100;
+        if (perc != 0)
+        {
+            res ~= LangStat(lang, perc);
+        }
+    }
+
+    return res.sort!((a, b) => a.perc > b.perc).array();
+}
--- source/pastemyst/paste/package.d
+++ source/pastemyst/paste/package.d
@@ -3,3 +3,4 @@ module pastemyst.paste;
 public import pastemyst.paste.create;
 public import pastemyst.paste.remove;
 public import pastemyst.paste.tags;
+public import pastemyst.paste.langs;

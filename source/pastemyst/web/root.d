42b4bb292158a72c798c8f0b99dc098016f67d55
CodeMyst <code@myst.rs>
2020-07-29 20:52:50 +0200

    clone button

--- source/pastemyst/web/root.d
+++ source/pastemyst/web/root.d
@@ -30,4 +30,41 @@ public class RootWeb
 
         render!("home.dt", session, user);
     }
+
+    /++
+     + GET /id={id}
+     +
+     + home page with the specified paste auto filled in (clone)
+     +/
+    @queryParam("id", "id")
+    @path("/")
+    public void getHome(string id, HTTPServerRequest req)
+    {
+        import pastemyst.db : findOneById;
+
+        UserSession session = UserSession.init;
+        User user = User.init;
+
+        if (req.session && req.session.isKeySet("user"))
+        {
+            session = req.session.get!UserSession("user");    
+            user = findOneById!User(session.user.id);
+        }
+
+        const pasteRes = findOneById!Paste(id);
+
+        if (pasteRes.isNull)
+        {
+            return;
+        }
+
+        const paste = pasteRes.get();
+
+        if (paste.isPrivate && (paste.ownerId != session.user.id))
+        {
+            return;
+        }
+
+        render!("home.dt", session, user, paste);
+    }
 }

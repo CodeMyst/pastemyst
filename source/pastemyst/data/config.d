e6703757295b3070dfad402687b7ef8d424f1fd0
codemyst <code@myst.rs>
2021-03-18 15:37:37 +0100

    cleaned up the config system
    * moved submodules into subdirectory

--- source/pastemyst/data/config.d
+++ source/pastemyst/data/config.d
@@ -1,6 +1,7 @@
 module pastemyst.data.config;
 
 import dyaml;
+import yamlserialized;
 
 /++
  + struct holding the whole config file
@@ -25,21 +26,25 @@ public struct Config
     /++
      + is the current instance a development one
      +/
+    @YamlField("development_instance")
     public bool devInstance;
 
     /++
      + ip on where to host pastemyst
      +/
+    @YamlField("host_ip")
     public string hostIp;
 
     /++
      + on which port to host pastemyst
      +/
+    @YamlField("host_port")
     public ushort hostPort;
 
     /++
      + mongodb connection string
      +/
+    @YamlField("mongo_connection")
     public string mongoConnection;
 }
 
@@ -117,44 +122,6 @@ static this()
         	throw e;
         }
 
-        // TODO: clean this up
-
-        enforce(cfg.isValid(), format!"%s: invalid"(configName));
-
-        enforce(cfg.containsKey("github"), format!"%s: missing github"(configName));
-
-        enforce(cfg["github"].containsKey("id"), format!"%s: missing github id"(configName));
-        enforce(cfg["github"].containsKey("secret"), format!"%s: missing github secret"(configName));
-
-        _config.github.id = cfg["github"]["id"].as!string();
-        _config.github.secret = cfg["github"]["secret"].as!string();
-
-        enforce(cfg.containsKey("gitlab"), format!"%s: missing gitlab"(configName));
-
-        enforce(cfg["gitlab"].containsKey("id"), format!"%s: missing gitlab id"(configName));
-        enforce(cfg["gitlab"].containsKey("secret"), format!"%s: missing gitlab secret"(configName));
-
-        _config.gitlab.id = cfg["gitlab"]["id"].as!string();
-        _config.gitlab.secret = cfg["gitlab"]["secret"].as!string();
-
-        enforce(cfg.containsKey("hostname"), format!"%s: missing hostname"(configName));
-
-        _config.hostname = cfg["hostname"].as!string();
-
-        enforce(cfg.containsKey("development_instance"), format!"%s: missing development_instance"(configName));
-
-        _config.devInstance = cfg["development_instance"].as!bool();
-
-        enforce(cfg.containsKey("host_ip"), format!"%s: missing host_ip"(configName));
-
-        _config.hostIp = cfg["host_ip"].as!string();
-
-        enforce(cfg.containsKey("host_port"), format!"%s: missing host_port"(configName));
-
-        _config.hostPort = cfg["host_port"].as!ushort();
-
-        enforce(cfg.containsKey("mongo_connection"), format!"%s: missing mongo_connection"(configName));
-
-        _config.mongoConnection = cfg["mongo_connection"].as!string();
+        cfg.deserializeInto(_config);
     }
 }

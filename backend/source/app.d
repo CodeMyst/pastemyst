0df5cde62537a17c5d5cfea4fc928eea3447a88c
codemyst <code@myst.rs>
2021-04-28 21:37:24 +0200

    basic project setup

---
+++ backend/source/app.d
@@ -0,0 +1,18 @@
+import vibe.d;
+
+void main()
+{
+    auto router = new URLRouter();
+
+    auto fsettings = new HTTPFileServerSettings();
+    fsettings.serverPathPrefix = "/static";
+
+    router.get("/static/*", serveStaticFiles("../public/", fsettings));
+
+    auto serverSettings = new HTTPServerSettings();
+    serverSettings.bindAddresses = ["127.0.0.1"];
+    serverSettings.port = 5005;
+
+    listenHTTP(serverSettings, router);
+    runApplication();
+}
---
+++ frontend/src/App.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import Header from "./components/Header.svelte";
+</script>
+
+<style>
+</style>
+
+<main>
+    <Header />
+</main>

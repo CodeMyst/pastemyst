7de2085f2c929aab4f3e0cbf57a31c24c913dc91
CodeMyst <code@myst.rs>
2021-08-21 21:53:59 +0200

    reordering tabs with sortablejs

---
+++ frontend/src/components/Tab.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+    import { createEventDispatcher, onMount } from "svelte";
+
+    export let id: String;
+    export let active: boolean = false;
+    export let title: String = "untitled";
+
+    let dispatch = createEventDispatcher();
+
+    const onClick = (event: MouseEvent) => {
+        dispatch("click", { event: event });
+    };
+
+    const onClose = () => {
+        dispatch("close");
+    };
+</script>
+
+<div
+    class="tab"
+    class:active
+    on:click={onClick}
+    data-id={id}
+>
+    <span class="title">{title}</span>
+    <span class="close-icon" on:click={onClose}>
+        <ion-icon name="close" />
+    </span>
+</div>
+
+<style>
+    .tab {
+        background-color: var(--color-cod-gray-light);
+        padding: 0.5em 1em;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        cursor: pointer;
+        border-bottom: 1px solid var(--color-cod-gray-light);
+        user-select: none;
+    }
+
+    .tab.active {
+        border-color: var(--color-main);
+    }
+
+    .tab:hover {
+        background-color: var(--color-mineshaft);
+    }
+
+    .tab:first-child {
+        border-radius: var(--border-radius) 0 0;
+    }
+
+    .tab .title {
+        margin-right: 0.75em;
+    }
+
+    .tab .close-icon {
+        cursor: pointer;
+        padding: 0.25em;
+        margin: -0.25em;
+        border-radius: var(--border-radius);
+    }
+
+    .tab .close-icon:hover {
+        background-color: var(--color-cod-gray);
+    }
+</style>

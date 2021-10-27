a665e977f06e0ab51e7035cd6bfbdee0cf8d4437
CodeMyst <code@myst.rs>
2021-10-27 20:50:24 +0200

    better footer on mobile

--- frontend/src/components/Footer.svelte
+++ frontend/src/components/Footer.svelte
@@ -3,24 +3,29 @@
         © <a href="https://myst.rs/">codemyst</a> 2021
     </div>
 
-    <div class="version">
-        v3.0.0-alpha
-    </div>
+    <div class="version">v3.0.0-alpha</div>
 
     <div class="paste-count">0 currently active pastes</div>
 </footer>
 
-<style>
+<style lang="scss">
     footer {
         border-top: 2px solid var(--color-mineshaft);
         display: flex;
-        flex-direction: row;
+        flex-direction: column;
         justify-content: space-between;
-        padding: 1em;
+        padding: 1em 0;
         flex-shrink: 0;
         max-width: 90%;
         width: 1280px;
         margin: 0 auto;
         margin-top: 2em;
+        line-height: 1.5;
+    }
+
+    @media screen and (min-width: 640px) {
+        footer {
+            flex-direction: row;
+        }
     }
 </style>
